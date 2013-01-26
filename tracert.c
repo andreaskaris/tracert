1348a7c4f1fc126587ec8045d684fbf2859df7df
kreeze <ak.karis@gmail.com>
2013-01-26 01:41:04 -0500

    Ping done
    First try of traceroute works (does not yet timeout if no reply)

--- tracert.c
+++ tracert.c
@@ -11,14 +11,19 @@
 #include <time.h>
 #include <strings.h>
 
-typedef long int u_sec;
+#define MAX_PACKET_SIZE 100  
+
+typedef double u_sec; //microseconds
 typedef struct { 
-  u_sec rrt; 
+  u_sec rtt; 
   unsigned short ttl; 
   unsigned short seq_num; 
+  unsigned short tot_len;
+  char *saddr;
+  char *icmp_reply;
 } ping;
 
-int send_ping(const char * destination, unsigned short icmp_id, unsigned short icmp_seq_no, ping *p);
+int send_ping(const char * destination, unsigned short icmp_id, unsigned short icmp_seq_no, unsigned short ttl, ping *p);
 unsigned short in_checksum(unsigned short *addr, int len);
  
 int main(int argc, char *argv[]) {
@@ -27,29 +32,59 @@ int main(int argc, char *argv[]) {
 
   srand ( time(NULL) );
   unsigned short id = rand();
-  for(seq_no=1;seq_no <= 10;seq_no++) { 
-    send_ping(argv[1], id, seq_no, &p);
+  unsigned short ttl = 1;
+  for(seq_no=1;seq_no <= 10;seq_no++) {
+    ttl = seq_no;
+    send_ping(argv[1], id, seq_no, ttl, &p);
+   
+    printf("%d bytes from %s: icmp_seq=%d, ttl=%d, time=%.6f ms\n", 
+	   p.tot_len,
+	   p.saddr,
+	   p.seq_num,
+	   p.ttl,
+	   p.rtt
+	   );
+
+    usleep(1000000);
   }
 
   return 0;
 }
 
-int send_ping(const char * destination, unsigned short icmp_id, unsigned short icmp_seq_no, ping *p) {
+int send_ping(const char * destination, unsigned short icmp_id, unsigned short icmp_seq_no, unsigned short ttl, ping *p) {
   int sock;
+
+  //src and dst
   //int nsource = inet_addr(source);
   int ndestination = inet_addr(destination);
-  char * packet = malloc(sizeof(struct iphdr) + sizeof(struct icmphdr));
-  unsigned short packet_size = sizeof(struct iphdr) + sizeof(struct icmphdr);
-  struct iphdr * ip_header = (struct iphdr *) packet;
-  struct icmphdr * icmp_header = (struct icmphdr *) (packet + sizeof(struct iphdr));
 
+  //round-trip time for one packet in ms
+  double rtt;
+  struct timespec ping_start, ping_end;
+
+  //for packet
+  char * packet;
+  struct iphdr * ip_header; 
+  struct icmphdr * icmp_header;
+
+  //where do we send (yes, we have to provide dst twice, here and in ip_header)
+  struct sockaddr_in dst;
+  dst.sin_family = AF_INET;
+  dst.sin_addr.s_addr = ndestination;
+
+  //allocate memory for packet
+  packet = malloc(sizeof(struct iphdr) + sizeof(struct icmphdr));
+  ip_header = (struct iphdr *) packet;
+  icmp_header = (struct icmphdr *) (packet + sizeof(struct iphdr));
+
+  //ip header configuration, see http://tools.ietf.org/html/rfc791
   ip_header->version = 4;
   ip_header->ihl = 5; //internet header length in byte
   ip_header->tos = 0;
-  ip_header->tot_len = packet_size; //total length
-  ip_header->id = htons(random());
+  ip_header->tot_len = sizeof(struct iphdr) + sizeof(struct icmphdr); //total length
+  ip_header->id = random();
   ip_header->frag_off = 0;
-  ip_header->ttl = 255;
+  ip_header->ttl = ttl; 
   ip_header->protocol = IPPROTO_ICMP;
   ip_header->check = 0; 
   //ip_header->saddr = nsource; //we don't need a source address - this is actually set automatically by the socket
@@ -65,37 +100,41 @@ int send_ping(const char * destination, unsigned short icmp_id, unsigned short i
   icmp_header->un.echo.sequence = icmp_seq_no; //sequence number
 
   icmp_header->checksum = in_checksum((unsigned short *) icmp_header, sizeof(struct icmphdr));
-
+  
+  //create socket
   if( (sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1 ) {
     perror("Error opening socket");
     exit(1);
   }
 
-  /* 
-   *  IP_HDRINCL must be set on the socket so that
-   *  the kernel does not attempt to automatically add
-   *  a default ip header to the packet
-   */
-  int optval;
-  setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(optval));
+  //do NOT automatically add an IP header; we already defined on
+  int is_header_included = 1; //do NOT include header
+  setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &is_header_included, sizeof(int));
 
-  struct sockaddr_in dst;
-  dst.sin_family = AF_INET;
-  dst.sin_addr.s_addr = ndestination;
+  //send out
+  clock_gettime(CLOCK_REALTIME, &ping_start);
   if( sendto(sock, packet, ip_header->tot_len, 0, (struct sockaddr *) &dst, sizeof(dst)) == -1 ) {
     perror("Sending icmp");
     exit(1);
   }
 
-  printf("sizeof: %d\n", packet_size);
-
-  bzero(packet, packet_size); //reset packet to 0
-  if( recv(sock, packet, sizeof(struct iphdr) + sizeof(struct icmphdr), 0) == -1 ) {
+  //get reply; we write back to packet, so all pointers now point to the reply
+  if( recv(sock, packet, ip_header->tot_len, 0) == -1 ) {
     perror("Receiving answer");
     exit(1);
   }
-  //left off here
-  printf("%d bytes from %s: icmp_seq=%d, ttl=%d, time=%.3f ms\n", ip_header->tot_len, ip_header->saddr, icmp_header->un.echo.sequence, 64, 0.123); 
+  clock_gettime(CLOCK_REALTIME, &ping_end);
+  rtt = (ping_end.tv_sec - ping_start.tv_sec) * 1000
+    + 
+    (ping_end.tv_nsec - ping_start.tv_nsec) / 1E6;
+
+  struct in_addr saddr = { ip_header->saddr  };
+  p->seq_num = icmp_header->un.echo.sequence;
+  p->ttl = ip_header->ttl;
+  p->rtt = rtt;
+  p->icmp_reply = packet;
+  p->tot_len = ntohs(ip_header->tot_len);
+  p->saddr = inet_ntoa(saddr);
 
   return 0;
 }
